fc35c3b330b6d5a2261c4e2cfa0df61c931dda3c
Kevin <kevinosborn31@gmail.com>
2023-09-26 12:38:06 +1000

    Date picker and gitignore

--- client/src/pages/ManagementPage.tsx
+++ client/src/pages/ManagementPage.tsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // Import the styles
+import { Box, TextField } from '@mui/material';
 
 const ManagementPage = () => {
+  const [date, setDate] = useState(new Date());
 
-    return (
-        <p>Glucose Content</p>
-    );
+  const handleDateChange = (newDate: Date) => {
+    setDate(newDate);
+  };
+
+  return (
+    <Box>
+      <DatePicker
+        selected={date}
+        onChange={handleDateChange}
+        dateFormat="MMMM d, yyyy" // Format to display the date
+        customInput={<TextField />} // Use Material-UI TextField as the input
+      />
+    </Box>
+  );
 }
 
-export default ManagementPage
+export default ManagementPage;
